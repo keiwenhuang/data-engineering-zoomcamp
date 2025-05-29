481e1cb5e6bbd2cc518989dbe5e1a80eea2542b2
keiwenhuang <kaoghk@gmail.com>
2025-05-28 17:46:59 -0700

    module 4 setup

--- 03-data-warehouse/extras/web_to_gcs.py
+++ 03-data-warehouse/extras/web_to_gcs.py
@@ -1,7 +1,8 @@
 import io
 import os
-import requests
+
 import pandas as pd
+import requests
 from google.cloud import storage
 
 """
@@ -12,9 +13,9 @@ Pre-reqs:
 """
 
 # services = ['fhv','green','yellow']
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
+init_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/"
 # switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "refined-cortex-460500-i2-kestra")
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -23,10 +24,11 @@ def upload_to_gcs(bucket, object_name, local_file):
     """
     # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
     # # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
 
-    client = storage.Client()
+    CREDENTIALS_FILE = "gcs.json"
+    client = storage.Client.from_service_account_json(CREDENTIALS_FILE)
     bucket = client.bucket(bucket)
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
@@ -34,9 +36,9 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 def web_to_gcs(year, service):
     for i in range(12):
-        
+
         # sets the month part of the file_name string
-        month = '0'+str(i+1)
+        month = "0" + str(i + 1)
         month = month[-2:]
 
         # csv file_name
@@ -45,22 +47,21 @@ def web_to_gcs(year, service):
         # download it using requests via a pandas df
         request_url = f"{init_url}{service}/{file_name}"
         r = requests.get(request_url)
-        open(file_name, 'wb').write(r.content)
+        open(file_name, "wb").write(r.content)
         print(f"Local: {file_name}")
 
         # read it back into a parquet file
-        df = pd.read_csv(file_name, compression='gzip')
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
+        df = pd.read_csv(file_name, compression="gzip")
+        file_name = file_name.replace(".csv.gz", ".parquet")
+        df.to_parquet(file_name, engine="pyarrow")
         print(f"Parquet: {file_name}")
 
-        # upload it to gcs 
+        # upload it to gcs
         upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
         print(f"GCS: {service}/{file_name}")
 
 
-web_to_gcs('2019', 'green')
-web_to_gcs('2020', 'green')
+web_to_gcs("2019", "fhv")
+# web_to_gcs("2020", "fhv")
 # web_to_gcs('2019', 'yellow')
 # web_to_gcs('2020', 'yellow')
-
