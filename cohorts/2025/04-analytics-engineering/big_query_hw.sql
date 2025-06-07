22874f7bba7523ae4703d6a3cc4a83c8df154270
keiwenhuang <kaoghk@gmail.com>
2025-06-06 17:07:00 -0700

    setup for homework 4

---
+++ cohorts/2025/04-analytics-engineering/big_query_hw.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE EXTERNAL TABLE `refined-cortex-460500-i2.hw4.fhv_tripdata_ext`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://refined-cortex-460500-i2-kestra/fhv/fhv_tripdata_2019-*.parquet']
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `refined-cortex-460500-i2.hw4.green_tripdata_ext`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://refined-cortex-460500-i2-kestra/green/green_tripdata_*.parquet']
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `refined-cortex-460500-i2.hw4.yellow_tripdata_ext`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://refined-cortex-460500-i2-kestra/yellow/yellow_tripdata_*.parquet']
+);
